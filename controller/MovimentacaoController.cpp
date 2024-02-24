1762bbdf8ee826059324ff8092255a5ae5054e7e
Pedrokusiak <pedro01kusiak@gmail.com>
2024-02-23 21:56:43 -0300

    Modificação MovimentacaoController

--- controller/MovimentacaoController.cpp
+++ controller/MovimentacaoController.cpp
@@ -3,32 +3,48 @@
 
 class MovimentacaoController{
 
-void teclaPraCima(SDL_KeyboardEvent *event);
-void teclaPraBaixo(SDL_KeyboardEvent *event);
+void teclaParaCima(SDL_KeyboardEvent *event);
+void teclaParaBaixo(SDL_KeyboardEvent *event);
 
 
-void teclaPraCima(SDL_KeyboardEvent *event)
+void teclaParaCima(SDL_KeyboardEvent *event)
 {
 	if (event->repeat == 0)
 	{
 		if (event->keysym.scancode == SDL_SCANCODE_UP)
 		{
-			app.up = 1;
+			tela.setCima = 1;
 		}
 
 		if (event->keysym.scancode == SDL_SCANCODE_DOWN)
 		{
-			app.down = 1;
-		}
-
-		if (event->keysym.scancode == SDL_SCANCODE_LEFT)
-		{
-			app.left = 1;
+			tela.setBaixo = 1;
 		}
 
 		if (event->keysym.scancode == SDL_SCANCODE_RIGHT)
 		{
-			app.right = 1;
+			tela.direita = 1;
+		}
+	}
+}
+
+void teclaParaBaixo(SDL_KeyboardEvent *event)
+{
+	if (event->repeat == 0)
+	{
+		if (event->keysym.scancode == SDL_SCANCODE_UP)
+		{
+			tela.setCima = 1;
+		}
+
+		if (event->keysym.scancode == SDL_SCANCODE_DOWN)
+		{
+			tela.setBaixo = 1;
+		}
+
+		if (event->keysym.scancode == SDL_SCANCODE_RIGHT)
+		{
+			tela.direita = 1;
 		}
 	}
 }
