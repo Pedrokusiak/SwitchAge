f1ec4da04d615ce47c8eadee50592602da7de5a1
Pedrokusiak <pedro01kusiak@gmail.com>
2024-03-06 20:12:43 -0300

    Alterações MovimentaçãoController

---
+++ controller/InterfaceExemplo.h
@@ -0,0 +1,29 @@
+class InterfaceExemplo{
+    public:
+        virtual void teclaParaCima(){
+            jogo->teclaParaCima(); 
+        }
+        virtual void processarEventos() = 0;
+};
+
+class ImplementacaoExemplo : public InterfaceExemplo{
+    public:
+        void processarEventos() override{
+             SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                exit(0);
+                break;
+            case SDL_KEYDOWN:
+                doKeyDown(&event.key);
+                break;
+            case SDL_KEYUP:
+                teclaParaCima(); // Call doKeyUp function here
+                break;
+            default:
+                break;
+             }
+            }
+        };
+};
--- controller/MovimentoControllerInterface.hpp
+++ controller/MovimentoControllerInterface.hpp
@@ -2,7 +2,9 @@ class MovimentacaoController {
 private:
     TecladoInterface* teclado;
 public:
-    virtual void teclaParaCima(SDL_KeyboardEvent *event);
+    virtual void teclaParaCima(){
+        teclado -> teclaParaCima();
+    }
     virtual void teclaParaBaixo(SDL_KeyboardEvent *event);
     virtual void leEntrada(void);
     virtual void movimentaObjeto(Objeto objeto, Direcao direcao, int quantidade);
